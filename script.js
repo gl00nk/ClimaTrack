27ba80c0a9b14e1941d776d2c0a0419b6657dcde
gl00nk <candhee2013@gmail.com>
2025-02-21 23:09:00 +0800

    script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ let weather = {
         "Wind speed: " + speed + " km/h";
       document.querySelector(".weather").classList.remove("loading");
       document.body.style.backgroundImage =
-      "url('https://images.unsplash.com/photo-1486325212027-8081e485255e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" + name + "')";
+      "url('https://images.unsplash.com/photo-1486325212027-8081e485255e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  + "')";
     },
     search: function () {
       this.fetchWeather(document.querySelector(".search-bar").value);
